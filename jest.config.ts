fbcb7d9574e64b2402e931bda45a839bbe2f9ea4
Vergiliogu <vergiliopoleza@gmail.com>
2024-05-12 00:22:15 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  rootDir: "./",
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  collectCoverage: true,
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/test/",
+    "/dist/",
+  ],
+  coverageDirectory: "./coverage",
+  coverageProvider: "v8",
+  testEnvironment: "node",
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  }
+};
+
+export default config;
