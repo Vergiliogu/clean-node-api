c441c9e299658665877738351001f99b5c30a23a
Vergiliogu <vergiliopoleza@gmail.com>
2024-05-17 11:48:55 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,31 @@
+import { MongoClient } from 'mongodb'
+import { MongoHelper } from '../helper/mongo-helper';
+import { AccountMongoRepository } from './account';
+
+describe('Account Mongo Repository', () => {
+  let client: MongoClient;
+
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL!)
+    client = MongoHelper.client
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'any',
+      email: 'any@mail.com',
+      password: 'any'
+    })
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any')
+    expect(account.email).toBe('any@mail.com')
+    expect(account.password).toBe('any')
+  });
+});
