8bf966399e8ddaffdd9d490f428eab0c025f2f57
Vergiliogu <vergiliopoleza@gmail.com>
2024-05-18 17:22:33 -0300

    feat: ensure requests return json as default content type

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,10 +1,12 @@
 import {Express} from 'express'
 import { bodyParser } from './middlewares/body-parser'
 import { cors } from './middlewares/cors'
+import { contentType } from './middlewares/content-type'
 
 export const middlewares = (app: Express): void => {
   app.use(
     bodyParser,
     cors,
+    contentType,
   )
 }
--- src/main/config/middlewares/cors.ts
+++ src/main/config/middlewares/cors.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
-export const cors = (req: Request, res: Response, next: NextFunction) => {
+export const cors = (_req: Request, res: Response, next: NextFunction) => {
   res.set('access-control-allow-origin', '*')
   res.set('access-control-allow-methods', '*')
   res.set('access-control-allow-headers', '*')
