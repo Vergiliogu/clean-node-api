865267704cd628600708182b8318921c42878c93
Vergiliogu <vergiliopoleza@gmail.com>
2024-05-18 17:15:03 -0300

    feat: ensure CORS is enabled

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import {Express} from 'express'
 import { bodyParser } from './middlewares/body-parser'
+import { cors } from './middlewares/cors'
 
 export const middlewares = (app: Express): void => {
   app.use(
-    bodyParser
+    bodyParser,
+    cors,
   )
 }
