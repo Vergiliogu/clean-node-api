dc508c17749ffcf7d847b7186169dc3b229131e3
Vergiliogu <vergiliopoleza@gmail.com>
2024-05-19 13:49:51 -0300

    refactor: change ** to **

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -5,7 +5,7 @@ export const setupRoutes = (app: Express): void => {
   const router = Router()
   app.use('/api', router)
 
-  sync('**/src/main/config/routes/**routes.ts')
+  sync('**/src/main/config/routes/*routes.ts')
     .map(async (file) => {
       console.log(file);
       (await import(`./routes/${file.split('/').at(-1)}`)).default(router)
