92ee19892a97a9e322cd1d1dd734c00efdde65bb
Vergiliogu <vergiliopoleza@gmail.com>
2024-05-19 13:56:06 -0300

    refactor: add clearColletion to MongoHelper

--- src/infra/db/mongodb/helper/mongo-helper.ts
+++ src/infra/db/mongodb/helper/mongo-helper.ts
@@ -15,6 +15,11 @@ export const MongoHelper = {
     return this.client.db().collection<T>(name)
   },
 
+  async clearCollection(name: string): Promise<void> {
+    const collection = this.getCollection(name)
+    await collection.deleteMany({})
+  },
+
   map<T>(
     collection: WithId<T>
   ): T & { id: string } {
